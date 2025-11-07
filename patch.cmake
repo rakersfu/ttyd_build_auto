d1754c2f5cca578173d69ef5a7f2faaf2929c5c5
rake_asus <furuijun@qq.com>
2025-11-07 19:33:55 +0800

    first commit

---
+++ patch.cmake
@@ -0,0 +1,8 @@
+target_link_libraries(ttyd
+  /opt/libwebsockets/lib/libwebsockets.a
+  /opt/libuv/lib/libuv.a
+  /opt/json-c/lib/libjson-c.a
+  /opt/openssl/lib/libssl.a
+  /opt/openssl/lib/libcrypto.a
+  /usr/lib/x86_64-linux-gnu/libz.a
+  pthread)
